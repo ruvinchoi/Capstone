cff5e3444ea28ead50ba3260ddd3be7e1e313891
ruvinchoi <107973538+ruvinchoi@users.noreply.github.com>
2025-05-06 20:18:20 -0700

    Add files via upload

---
+++ scripts/StaticBody2D.gd
@@ -0,0 +1,14 @@
+extends StaticBody2D
+
+@onready var collision_polygon_2d = $CollisionPolygon2D
+@onready var polygon_2d = $Polygon2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	polygon_2d.polygon = collision_polygon_2d.polygon
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
