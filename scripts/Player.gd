5dc5fa54b40f13b7b52327bdbceecec2804382ca
Ruvin Choi <wafer@Ruvins-MacBook-Air.local>
2025-05-12 12:37:35 -0700

    elevator

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 600.0
 const JUMP_VELOCITY = -500.0
+var onPlatform = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -26,3 +27,9 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+
+func _on_area_2d_area_entered(body):
+	if body.is_in_group("platform"):
+		onPlatform = true
+		
---
+++ scripts/pelevate.gd
@@ -0,0 +1,31 @@
+extends AnimatableBody2D
+
+@onready var wait = $wait
+var within = false
+var animPlaying = false
+
+func _ready():
+	pass 
+
+func _physics_process(_delta):
+	testanimate()
+
+func testanimate():
+	if Input.is_action_just_pressed("test") and within == true and $AnimationPlayer.is_playing() == false and animPlaying == false:
+		$AnimationPlayer.play("up")
+		animPlaying = true
+		wait.start()
+	elif Input.is_action_just_pressed("shift"):
+		$AnimationPlayer.play("down")
+	
+func _on_wait_timeout():
+	$AnimationPlayer.play("down")
+	animPlaying = false
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("player"):
+		within = true
+		
+func _on_area_2d_body_exited(body):
+		if body.is_in_group("player"):
+			within = false
